f19ebcd5c23311ca8cb550c934d569243700cd5b
Vítor <vitor.mlps@gmail.com>
2022-11-12 11:09:30 -0300

    commit update - atualização da estrutura html/css

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/autenticacao'));
 app.use(express.static(__dirname + '/dashboard'));
 app.use(express.static(__dirname + '/login'));
-app.use(express.static(__dirname + '/servicos'));
+// app.use(express.static(__dirname + '/servicos'));
 console.log('Listening on 8001');
 app.listen(8001);
 
